61dd500ef48c9c43d58cf112eb51f185e5c01cee
Bhavana <bhavanakondeti2000@gmail.com>
2026-05-19 12:47:17 -0500

    Added local storage

--- script.js
+++ script.js
@@ -1,22 +1,52 @@
 document.addEventListener("click",(e)=>{
-    const btn = e.target.closest(".toggle");
-
+    const btn=e.target.closest(".toggle");
     if(!btn) return;
-
-    const id = btn.getAttribute("aria-controls");
-
-    const section = document.getElementById(id);
-
+    const id=btn.getAttribute("aria-controls");
+    const section=document.getElementById(id);
     section.classList.toggle("open");
 
-    btn.setAttribute("aria-expanded",section.classList.contains("open"));
+    btn.setAttribute(
+        "aria-expanded",
+        section.classList.contains("open")
+    );
 });
-document.addEventListener("change",updateProgress);
-updateProgress();
-function updateProgress()
-{
-    const total = document.querySelectorAll("input").length;
-    const complete = document.querySelectorAll("input:checked").length;
 
-    document.querySelector(".progress").innerText = `${complete}/${total} complete`;
+
+document.addEventListener("change",updateProgress);
+
+
+updateProgress();
+
+
+function updateProgress(){
+
+const total=document.querySelectorAll("input").length;
+
+const complete=document.querySelectorAll("input:checked").length;
+
+document.querySelector(".progress").innerText=`${complete}/${total} complete`;}
+
+
+
+document.addEventListener("change",
+(e)=>{
+if(e.target.matches("input")){
+const states=[];
+
+document.querySelectorAll("input").forEach(c=>{
+states.push(c.checked);
+});
+
+localStorage.setItem("tasks",JSON.stringify(states));
 }
+
+});
+
+
+
+const saved=JSON.parse(localStorage.getItem("tasks"))||[];
+
+document.querySelectorAll("input")
+.forEach((c,i)=>{
+c.checked=saved[i];
+});
